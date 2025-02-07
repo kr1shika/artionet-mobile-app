9c1b1aeec4fcda655da2c0fac600d100fe7e62fc
krishika <krishikakh@gmail.com>
2025-02-07 19:10:54 +0545

    unit Testing[Mockito]

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,28 +1,57 @@
-class Failure {
+// import 'package:equatable/equatable.dart';
+
+// abstract class Failure extends Equatable {
+//   final String message;
+
+//   const Failure({required this.message});
+
+//   @override
+//   List<Object> get props => [message];
+// }
+
+// class LocalDatabaseFailure extends Failure {
+//   const LocalDatabaseFailure({required super.message});
+// }
+
+// class ApiFailure extends Failure {
+//   final int? statusCode;
+//   const ApiFailure({
+//     this.statusCode,
+//     required super.message,
+//   });
+// }
+
+// class SharedPrefsFailure extends Failure {
+//   const SharedPrefsFailure({
+//     required super.message,
+//   });
+// }
+
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
   final String message;
-  final int? statusCode;
-  Failure({
-    required this.message,
-    this.statusCode,
-  });
+
+  const Failure({required this.message});
 
   @override
-  String toString() => 'Failure(message: $message, statusCode: $statusCode)';
+  List<Object> get props => [message];
 }
 
-// local database ko failure
-class LocaldatabaseFailure extends Failure {
-  LocaldatabaseFailure({
+class LocalDatabaseFailure extends Failure {
+  const LocalDatabaseFailure({required super.message});
+}
+
+class ApiFailure extends Failure {
+  final int? statusCode;
+  const ApiFailure({
+    this.statusCode,
     required super.message,
   });
 }
 
-// api failure
-class ApiFailure extends Failure {
-  @override
-  final int? statusCode;
-  ApiFailure({
-    this.statusCode,
+class SharedPrefsFailure extends Failure {
+  const SharedPrefsFailure({
     required super.message,
   });
 }
